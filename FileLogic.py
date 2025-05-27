aecb275cc3e186b8846cfd124eb4d100503b30e6
Bryan Kim <wabkim360@gmail.com>
2025-05-26 18:23:05 -0700

    added file logic

--- FileLogic.py
+++ FileLogic.py
@@ -1,2 +1,23 @@
+import os
 # input logic (user input, save filename)
+
+# pre : filepath must be in the Desktop folder on MacOS
+# post: returns contents of file 
+def get_file(filename):
+    full_path = "~/Desktop/" + filename
+    if not filename.endswith(".py") or not os.path.exists():
+        return []
+    with open(full_path, "r") as infile:
+        all_lines = infile.readlines()
+    return all_lines
+
 # output logic (default to filename_refactored.py)
+
+# pre : assumed to only be called when
+#       content is assumed to be correctly refactored in Processing
+# post: content written in filename_refactored.py
+def produce_refactored(filename, content):
+    produced = os.path.splitext(filename)[0] + "_refactored.py"
+    full_path = "~/Desktop/" + produced
+    with open(full_path, "w") as outfile:
+        outfile.writelines(content)
