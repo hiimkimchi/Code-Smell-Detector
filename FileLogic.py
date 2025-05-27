1be532e435a5c8ee7a80ec18beffbf2392194625
Bryan Kim <wabkim360@gmail.com>
2025-05-27 02:42:03 -0700

    refactored to take less loc

--- FileLogic.py
+++ FileLogic.py
@@ -1,7 +1,7 @@
 import os
 
-# pre : filepath must be in the Desktop folder on MacOS
-# post: returns contents of file 
+# pre : - filepath must be in the Desktop folder on MacOS
+# post: - returns contents of file 
 def get_file(filename):
     full_path = "~/Desktop/" + filename
     if not filename.endswith(".py") or not os.path.exists():
@@ -10,8 +10,8 @@ def get_file(filename):
         all_lines = infile.readlines()
     return all_lines
 
-# pre : assumed to only be called when content is assumed to be correctly refactored in Processing
-# post: content written in filename_refactored.py
+# pre : - assumed to only be called when content is assumed to be correctly refactored in Processing
+# post: - content written in filename_refactored.py
 def produce_refactored(filename, content):
     produced = os.path.splitext(filename)[0] + "_refactored.py"
     full_path = "~/Desktop/" + produced
--- Processing.py
+++ Processing.py
@@ -2,26 +2,44 @@ import re
 
 MAX_LOC = 15
 
-
-
-# long method (check for lines of code (no more than 15) from every "def", use regex)
-
-# pre :
-# post:
-def check_LOC (contents):
-    current_start_index = 0
-    long_methods = []
+# pre : - only called in check_LOC_and_params
+# post: - returns name of function (or returns none)
+def check_LOC (line, start_index, index):
     function_name_pattern = re.compile(r'^\s*def\s+([A-Za-z_]\w*)\s*\(')
-    for index in range(len(contents)):
-        if not contents[index].startswith("def "):
-            continue
-        function_name = function_name_pattern.match(contents[index])
-        LOC = index - current_start_index
-        if LOC > MAX_LOC:
-            long_methods.append(function_name)
-    return long_methods
+    function_name = function_name_pattern.match(line)
+    LOC = index - start_index
+    if LOC > MAX_LOC:
+        return function_name
+    return None
 
-# long parameter list (check line of def, use regex to see parameters within commas after the ( and before the ))
+# pre : - only called in check_LOC_and_params
+# post: - returns name of function (or returns none)
+def check_parameters (line):
+    print()
+
+# pre : - only called in read_lines
+# post: - returns result of check_LOC and result of check_parameters
+def check_LOC_and_params (contents, start_index, index):
+    return check_LOC(contents[start_index], start_index, index), check_parameters(contents[index])
+
+# pre : - contents are correctly read in FileLogic.py
+# post: - returns list of methods that are too long and methods that have too many parameters
+def read_lines (contents):
+    current_start_index, long_methods, long_parameters = None, [], []
+    for index in range(len(contents)):
+        if not contents[index].startswith("def ") or index < len(contents):
+            continue
+        if not current_start_index:
+            current_start_index = index
+        long_method, long_param_list = check_LOC_and_params(contents, current_start_index, index)
+        if long_method:
+            long_methods.append(long_method)
+        if long_param_list:
+            long_parameters.append(long_param_list)
+        current_start_index = index
+    return long_methods, long_parameters
+
+
 # duplicated code (use Jaccard Similarity for each method, compare two at a time)
 # refactor dup (whichever method is called more, replace lesser used ones with the more used ones, send to FileLogic)
 
